a55f2aa03721ed9c8ae167951b7408ef6b55e990
Franco <franconorona@outlook.com>
2025-05-11 09:49:37 -0300

    Add DTO

--- src/routes/sessions-router.js
+++ src/routes/sessions-router.js
@@ -1,4 +1,5 @@
 import { Router } from "express";
+import { UserDTO } from "../dto/user-dto.js";
 import passport from "../config/passport/passport.js";
 
 const router = Router();
@@ -7,14 +8,8 @@ router.get(
   "/current",
   passport.authenticate("jwt", { session: false }),
   (req, res) => {
-    const userData = {
-      id: req.user._id,
-      first_name: req.user.first_name,
-      last_name: req.user.last_name,
-      email: req.user.email,
-      role: req.user.role,
-    };
-    res.status(200).json({ user: userData });
+    const userDTO = new UserDTO(req.user);
+    res.status(200).json({ user: userDTO });
   }
 );
 
